2f352bed011c0a7255a149ef6552aac6d422c5e4
mani <manivanann.prushorth@kcl.ac.uk>
2024-07-01 22:20:36 -0700

    Add starter code

---
+++ travel-app/App/travel_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_appApp.swift
+//  travel-app
+//
+//  Created by Prushorth Manivannan on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct travel_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
